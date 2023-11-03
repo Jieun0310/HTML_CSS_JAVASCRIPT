0c4a7acb2f6151aacf56e0ca04d999ec5c8be083
cmkm3010 <cmkm8384@gmail.com>
2023-11-03 14:41:43 +0900

    init

---
+++ javascript/basic/09_object_string.js
@@ -0,0 +1,60 @@
+// 1.string object 문자열 객체
+{
+//     const str = 'I ate 김밥 for lunch. it was good 김밥'
+//     console.log(str.length)
+//     console.log(str.charAt(4)) //인덱스에 해당하는 문자 반환
+//     console.log(str.indexOf('김밥')) //제일 먼저 일치 -> 인덱스 반환
+//     console.log(str.lastIndexOf('김밥')) //오른쪽에서
+//     console.log(str.lastIndexOf('우동')) // 없으면 -1 반환
+//     console.log(str.replace('I','we')) // 찾아서 교체
+//     console.log(str.search('f')) // 인덱스 반환
+//     console.log(str.slice(4,10)) //a에서 b까지 반환
+// }
+
+// // 문자열을 배열로 만들기
+// {
+//     const str ='html,css,javascript'
+//     const result = str.split(',')
+//     console.log(result)
+// }
+
+// // 대소문자
+// {
+//     const str = 'hello'
+//     const str2 = 'WORLD'
+//     console.log(str.toUpperCase())
+//     console.log(str2.toLowerCase())
+// }
+
+// // 문자열 합치기
+// {
+//     const str = 'hello'
+//     const result = str.concat('world')
+//     console.log(result);
+}
+
+// 공백제거
+{
+    // const str =' hello world'
+    // const result = str.trim()
+    // console.log(str)
+    // console.log(result)
+}
+
+// 방문자의 이름을 받아서 대문자로 출력
+// 전화번호의 뒷자리 4개를 *로 출력
+{
+    const name = prompt('당신의 이름은?', '')
+    const phone = prompt('당신의 전화번호는?', '')
+
+    const p = '010-123-4567'
+
+    const upperName = name.toUpperCase()
+    const result = p.substring(0,phone.length-4) + '****'
+
+    document.write(upperName,'<br>')
+    document.write(result,'<br>')
+}
+
+
+
