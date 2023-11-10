ea1abddf74b5567d36dd234db26293283a8c0761
cmkm3010 <cmkm8384@gmail.com>
2023-11-10 12:07:50 +0900

    init

--- javascript/tutorial/09_Todolist/js/to.js
+++ javascript/tutorial/09_Todolist/js/to.js
@@ -1,65 +1,128 @@
+// const form = document.querySelector('.todo_form')
+// const input = document.querySelector('input')
+// const ul = document.querySelector('.todo_list')
+
+// let todos = []
+
+// function saveTodo(){
+//     localStorage.setItem('todos',json.strungify(todos))
+// }
+
+
+// function removeTodo(e){
+//     console.log(e)
+//     const li = e.target.parentNode
+//     li.remove()
+//     const newTodos = todos. filter(todo => todo.id !== parseInt(li.id))
+//     todos = newTodos
+// }
+
+// function paintTodo(text){
+//     console.log(text)
+
+//     const li = document.createElement('li')
+//     const span = document.createElement('span')
+
+//     li.innerText = text
+//     li.id = todos.length + 1
+//     span.innerText = 'x'
+//     span.addEventListener('click',removeTodo)
+
+//     li.appendChild(span)
+//     ul.appendChild(li)
+
+//     todos.push({
+//         id : todos.length + 1,
+//         text: text
+//     })
+
+//     saveTodo()
+// }
+
+// function handleSubmit(e){
+//     e.preventDefault()
+//     if(input.value ==='') return
+//     paintTodo(input.value)
+//     input.value = ''
+// }
+
+
+// function loadTodo(){
+//     const loadedTodos = localStorage.getItem('todos')
+    
+//     if(loadedTodos !== null){
+//         const parseTodo = JSON. parse(loadedTodos)
+//         parseTodo.forEach(todo => paintTodo(todo.text))
+//     }
+// }
+
+// function init(){
+//     loadTodo()
+//     form.addEventListener('submit',handleSubmit)
+// }
+
+// init()
+
+////
+
 const form = document.querySelector('.todo_form')
-const input = document.querySelector('input')
+const input = form.querySelector('input')
 const ul = document.querySelector('.todo_list')
 
 let todos = []
 
-function saveTodo(){
-    localStorage.setItem('todos',json.strungify(todos))
+function saveTodo() {
+    localStorage.setItem('todos', JSON.stringify(todos))
 }
 
-
-function removeTodo(e){
-    console.log(e)
+function removeTodo(e) {
     const li = e.target.parentNode
     li.remove()
-    const newTodos = todos. filter(todo => todo.id !== parseInt(li.id))
+    const newTodos = todos.filter(todo => todo.id !== parseInt(li.id))
     todos = newTodos
+    saveTodo()    
 }
 
-function paintTodo(text){
-    console.log(text)
-
+function paintTodo(text) {
     const li = document.createElement('li')
     const span = document.createElement('span')
 
     li.innerText = text
     li.id = todos.length + 1
+    
     span.innerText = 'x'
-    span.addEventListener('click',removeTodo)
+    span.addEventListener('click', removeTodo)
 
     li.appendChild(span)
     ul.appendChild(li)
 
     todos.push({
-        id : todos.length + 1,
+        id: todos.length + 1,
         text: text
     })
 
     saveTodo()
 }
 
-function handleSubmit(e){
+function handleSubmit(e) {
     e.preventDefault()
-    if(input.value ==='') return
+    if(input.value === '') return
     paintTodo(input.value)
     input.value = ''
 }
 
+function loadTodo() {
+    const loadTodos = localStorage.getItem('todos')
 
-function loadTodo(){
-    const loadedTodos = localStorage.getItem('todos')
-    
-    if(loadedTodos !== null){
-        const parseTodo = JSON. parse(loadedTodos)
+    if(loadTodos !== null) {
+        const parseTodo = JSON.parse(loadTodos)
         parseTodo.forEach(todo => paintTodo(todo.text))
     }
-
 }
 
-function init(){
+function init() {
     loadTodo()
-    form.addEventListener('submit',handleSubmit)
+    form.addEventListener('submit', handleSubmit)
 }
 
 init()
--- javascript/tutorial/09_Todolist/js/weather.js
+++ javascript/tutorial/09_Todolist/js/weather.js
@@ -1,42 +1,84 @@
-const weather = document.querySelector('.weather')
-const API = '8a9aa8bf6c87c66c6f6b3d0e7bbb9d84'
+// const weather = document.querySelector('.weather')
+// const API = '8a9aa8bf6c87c66c6f6b3d0e7bbb9d84'
 
-function getWeather(lat,lon) {
-    console.log(lat,lon);
+// function getWeather(lat,lon) {
+//     console.log(lat,lon);
+
+//     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API}&units=metric`
+
+//     fetch(url)
+//     .then(function(response) {
+//         return response.json()
+//     })
+//     .then(function(json){
+//         console.log(json)
+//         const city = json.name
+//         const temp = json.main.temp
+
+//         weather.innerText = `${temp}°C ${city} `
+//     })
+// }
+
+// function geoSuccess(position){
+//     const latitude = position.coords.latitude
+//     const longitude = position.coords.longitude
+
+//     // console.log(latitude,longitude);
+
+//     getWeather(latitude,longitude)
+// }
+
+// function geoError(){
+//     console.log('위치정보를 허용하지 않았습니다.')
+// }
+
+// function askCoords(){
+//     navigator.geolocation.getCurrentPosition(geoSuccess,geoError)
+// }
+
+// function init(){
+//     askCoords()
+// }
+
+// init()
+
+
+///
+const weather = document.querySelector('.weather')
+const API = '26a8f813a56d73684d04cdff5a1f7720'
+
+function getWeaher(lat, lon) {
 
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API}&units=metric`
 
     fetch(url)
     .then(function(response) {
         return response.json()
-    })
-    .then(function(json){
-        console.log(json)
+    }) 
+    .then(function(json) {        
         const city = json.name
         const temp = json.main.temp
 
-        weather.innerText =`${temp}℃ ${city}`
-    })
+        weather.innerText = `${temp}°C ${city} `
+    })   
 }
 
-function geoSuccess(position){
+function geoSucces(position) {
     const latitude = position.coords.latitude
-    const longitude = position.coords.longitude
+    const longitude = position.coords.longitude    
 
-    console.log(latitude,longitude);
-
-    getWeather(latitude,longitude)
+    getWeaher(latitude, longitude)
 }
 
-function geoError(){
-    console.log('위치정보를 허용하지 않았습니다.')
+function geoError() {
+    console.log('위치 정보를 허용하지 않았습니다.')
 }
 
-function askCoords(){
-    navigator.geolocation.getCurrentPosition(geoSuccess,geoError)
+function askCoords() {
+    navigator.geolocation.getCurrentPosition(geoSucces, geoError)
 }
 
-function init(){
+function init() {
     askCoords()
 }
 
